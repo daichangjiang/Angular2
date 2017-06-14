ce081e3832b7f0737c9a15fec40c7c39de8aa13f
unknown <changjiangd@tujia.com>
2017-06-14 14:07:37 +0800

    Angular2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+// import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { HotelSearchBarComponent } from './hotel-search-bar/hotel-search-bar.component';
+import { RevenueMainPanelComponent } from './revenue-main-panel/revenue-main-panel.component';
+import { FixedBottomMenuComponent } from './fixed-bottom-menu/fixed-bottom-menu.component';
+import { RevenueSummaryComponent } from './revenue-summary/revenue-summary.component';
+import { DatePriceSlideComponent } from './date-price-slide/date-price-slide.component';
+import { HotelSearchPageComponent } from './hotel-search-page/hotel-search-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: 'main', component: RevenueMainPanelComponent },
+  { path: 'summary', component: RevenueSummaryComponent }
+  // { path: 'contactus', redirectTo: 'contact' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotelSearchBarComponent,
+    RevenueMainPanelComponent,
+    FixedBottomMenuComponent,
+    RevenueSummaryComponent,
+    DatePriceSlideComponent,
+    HotelSearchPageComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
